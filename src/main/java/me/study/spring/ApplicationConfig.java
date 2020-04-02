df33e91d92db95b5ce5a04a18e6d7ae17a7a0836
Jeongsu Yang <mycat83@gmail.com>
2020-04-02 22:59:27 +0900

    Initial commit

---
+++ src/main/java/me/study/spring/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package me.study.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = StudySpringApplication.class)
+public class ApplicationConfig {
+
+}
