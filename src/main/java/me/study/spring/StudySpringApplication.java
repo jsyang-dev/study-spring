6a5d493dfffc20b48ddd603edbef686f2af885b6
Jeongsu Yang <mycat83@gmail.com>
2020-04-25 13:55:03 +0900

    IoC 컨테이너 7부: MessageSource

--- src/main/java/me/study/spring/StudySpringApplication.java
+++ src/main/java/me/study/spring/StudySpringApplication.java
@@ -2,7 +2,10 @@ package me.study.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties")
@@ -10,6 +13,15 @@ public class StudySpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudySpringApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+        return messageSource;
     }
 
 }
