419f107e394422136c8a21156190bb8aad8dfa55
Jeongsu Yang <mycat83@gmail.com>
2020-04-25 21:40:36 +0900

    IoC 컨테이너 8부: ApplicationEventPublisher

--- src/main/java/me/study/spring/AppRunner.java
+++ src/main/java/me/study/spring/AppRunner.java
@@ -3,24 +3,17 @@ package me.study.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationEventPublisher publisher;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(messageSource.getClass());
-        while (true) {
-            System.out.println(messageSource.getMessage("greeting", new String[]{"yangjs"}, Locale.KOREA));
-            System.out.println(messageSource.getMessage("greeting", new String[]{"yangjs"}, Locale.getDefault()));
-            Thread.sleep(1000L);
-        }
+        publisher.publishEvent(new MyEvent(this, 100));
     }
 }
--- src/main/java/me/study/spring/StudySpringApplication.java
+++ src/main/java/me/study/spring/StudySpringApplication.java
@@ -2,26 +2,14 @@ package me.study.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@PropertySource("classpath:/app.properties")
+@EnableAsync
 public class StudySpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudySpringApplication.class, args);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setCacheSeconds(3);
-        return messageSource;
     }
 
 }
