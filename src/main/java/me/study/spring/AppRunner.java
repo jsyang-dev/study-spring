b299dddd18df1d0dbca123b26da3f36369f47d33
Jeongsu Yang <mycat83@gmail.com>
2020-04-26 11:41:50 +0900

    Resource 추상화

--- src/main/java/me/study/spring/AppRunner.java
+++ src/main/java/me/study/spring/AppRunner.java
@@ -3,8 +3,8 @@ package me.study.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -14,11 +14,15 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+
         Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(resource.getURI());
