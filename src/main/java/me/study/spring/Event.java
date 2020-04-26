880fefba16ac459e1d37a25bdcbb2da81051fe3c
Jeongsu Yang <mycat83@gmail.com>
2020-04-26 22:54:26 +0900

    데이터 바인딩 추상화: PropertyEditor

--- src/main/java/me/study/spring/Event.java
+++ src/main/java/me/study/spring/Event.java
@@ -1,21 +1,14 @@
 package me.study.spring;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-
 public class Event {
 
     Integer id;
 
-    @NotEmpty
     String title;
 
-    @Min(0)
-    Integer limit;
-
-    @Email
-    String email;
+    public Event(Integer id) {
+        this.id = id;
+    }
 
     public Integer getId() {
         return id;
@@ -33,19 +26,11 @@ public class Event {
         this.title = title;
     }
 
-    public Integer getLimit() {
-        return limit;
-    }
-
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
