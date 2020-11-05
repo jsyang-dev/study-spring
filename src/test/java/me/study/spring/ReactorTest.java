d16fafe360f18049cf064544532c5a4dff4fad31
Jeongsu Yang <mycat83@gmail.com>
2020-11-05 22:43:09 +0900

    Reactor - Merge

--- src/test/java/me/study/spring/ReactorTest.java
+++ src/test/java/me/study/spring/ReactorTest.java
@@ -123,4 +123,30 @@ class ReactorTest {
         }
         return List.of(s.toUpperCase(), Thread.currentThread().getName());
     }
+
+    @Test
+    void reactorTest07() {
+
+        Flux<Long> flux1 = Flux.interval(Duration.ofMillis(100)).take(10);
+        Flux<Long> flux2 = Flux.just(100L, 101L, 102L);
+
+        flux1.mergeWith(flux2)
+                .doOnNext(System.out::println)
+                .blockLast();
+
+        flux1.concatWith(flux2)
+                .doOnNext(System.out::println)
+                .blockLast();
+    }
+
+    @Test
+    void reactorTest08() {
+
+        Mono<Long> mono1 = Mono.just(1L);
+        Mono<Long> mono2 = Mono.just(2L);
+
+        Flux.concat(mono1, mono2)
+                .doOnNext(System.out::println)
+                .blockLast();
+    }
 }
